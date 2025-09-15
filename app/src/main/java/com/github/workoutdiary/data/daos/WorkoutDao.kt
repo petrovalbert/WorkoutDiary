096dc8ea1bb42bb189de27bea7f6845076061bee
Petrov <renokurax@gmail.com>
2025-09-15 17:29:49 +0300

    Created DAO for WorkoutEntry. Defined main methods: get all workouts, get a workout by id number, get count of workouts, get workouts by date

--- app/src/main/java/com/github/workoutdiary/data/daos/WorkoutDao.kt
+++ app/src/main/java/com/github/workoutdiary/data/daos/WorkoutDao.kt
@@ -2,6 +2,7 @@ package com.github.workoutdiary.data.daos
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Delete
 import androidx.room.Query
 import com.github.workoutdiary.data.entities.WorkoutEntry
 import com.github.workoutdiary.Constants
@@ -11,6 +12,22 @@ interface WorkoutDao {
     @Insert
     suspend fun insert(entry: WorkoutEntry): Long
 
+    @Delete
+    suspend fun delete(entry: WorkoutEntry): Int
+
+    // Get all workouts
     @Query("SELECT * FROM ${Constants.TablenameWorkout.NAME} ORDER BY date DESC")
     suspend fun getAllEntries(): List<WorkoutEntry>
+
+    // Get a workout by id number
+    @Query("SELECT * FROM ${Constants.TablenameWorkout.NAME} WHERE id = :id")
+    suspend fun getWorkoutByID(id: Long): WorkoutEntry?
+
+    // Get count of workouts
+    @Query("SELECT COUNT(*) FROM ${Constants.TablenameWorkout.NAME}")
+    suspend fun getWorkoutCount(): Int
+
+    // Get workouts by date
+    @Query("SELECT * FROM ${Constants.TablenameWorkout.NAME} WHERE date = :date")
+    suspend fun getWorkoutsByDate(date: Long): List<WorkoutEntry>
 }
