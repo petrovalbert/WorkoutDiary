b4d4547deeda0cf14cda3c6efa5573eba23689c7
Petrov <renokurax@gmail.com>
2025-09-18 08:50:59 +0300

    New branch without old garbage. Making Make Activity from scratch

--- app/src/main/java/com/github/workoutdiary/WorkoutApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.github.workoutdiary
-
-import android.app.Application
-import com.github.workoutdiary.data.AppDatabase
-
-class WorkoutApplication : Application() {
-    val database: AppDatabase by lazy { AppDatabase.getInstance(this) }
-}
